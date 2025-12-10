69db44e1e14b53b7e32708e613db3c7a4898e7fc
ilkesen <ilkinesen2019@gmail.com>
2025-12-10 21:41:01 +0300

    Create Student.java

---
+++ src/main/java/org/example/se302_examscheduler/Student.java
@@ -0,0 +1,17 @@
+package org.example.se302_examscheduler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private final String id;
+
+    public Student(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+}
