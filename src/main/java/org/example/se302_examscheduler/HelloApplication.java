1e5c51fb2b619dbedec7bbcb6dac4c58207d4a49
Başak Ece Gökalp <74381527+basakecegokalp@users.noreply.github.com>
2025-12-16 23:46:01 +0300

    HelloApplication commit

--- src/main/java/org/example/se302_examscheduler/HelloApplication.java
+++ src/main/java/org/example/se302_examscheduler/HelloApplication.java
@@ -2,22 +2,21 @@ package org.example.se302_examscheduler;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+        primaryStage.setTitle("Exam Scheduler");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
-// deneme
+
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
